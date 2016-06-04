3cd4f597bab24d5b62173fbe8429cac49c2a9078
Kay Cha <kay@hannal.net>
2016-06-04 18:29:28 +0900

    add choices argument for Post

--- blog/models.py
+++ blog/models.py
@@ -2,14 +2,24 @@ from django.db import models
 
 
 class Post(models.Model):
+    _status = (
+        ('opn', 'Opened', ),
+        ('clsd', 'Closed', ),
+        ('prvt', 'Privated', ),
+        ('scheduled', '예약', ),
+    )
     title = models.CharField(max_length=200)
     content = models.TextField()
-    tags = models.ManyToManyField('Tag')
+    tags = models.ManyToManyField('Tag', blank=True)
+    status = models.CharField(max_length=20, choices=_status,)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{}: {}'.format(self.pk, self.title)
+
+    class Meta:
+        ordering = ['-created_at', '-pk']
 
 
 class Comment(models.Model):
