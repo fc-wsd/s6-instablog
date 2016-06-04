1ed8ec532b85a98c07a8043b42e769377323adcb
Kay Cha <kay@hannal.net>
2016-06-04 16:49:25 +0900

    add Comment model

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,17 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return '{}: {}'.format(self.pk, self.title)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+
