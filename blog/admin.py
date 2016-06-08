1984dfbd9b8f3129afa35ba9a6d8ff2d4b36a0e0
commonlife <commonlife@gmail.com>
2016-06-08 17:09:52 +0900

    카테고리 추가

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 from .models import Post
 from .models import Comment
+from .models import Category
+from .models import Tag
 
 
 admin.site.register(Post)
 admin.site.register(Comment)
-
+admin.site.register(Category)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -3,14 +3,15 @@ from django.db import models
 
 class Post(models.Model):
     _status = (
-        ('opn', 'Opened', ),
-        ('clsd', 'Closed', ),
-        ('prvt', 'Privated', ),
-        ('scheduled', '예약', ),
+        ('opn', 'Open', ),
+        ('cls', 'Close', ),
+        ('prvt', 'Private', ),
+        ('rsv', 'Reserved', ),
     )
     title = models.CharField(max_length=200)
     content = models.TextField()
     tags = models.ManyToManyField('Tag', blank=True)
+    category = models.ForeignKey('Category')
     status = models.CharField(max_length=20, choices=_status,)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -35,6 +36,13 @@ class Comment(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=40)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
 
     def __str__(self):
         return self.name
