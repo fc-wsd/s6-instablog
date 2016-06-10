252af2c8a62239b8449e8ea9489e085f46676f77
Jaemin <jaemin0216@gmail.com>
2016-06-10 20:53:42 +0900

    modify blog/admin.py and .gitignore

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,10 @@ from django.contrib import admin
 from .models import Post # . 의 의미는 현재 위치
 from .models import Comment
 from .models import Category
+from .models import Tag
 
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Category)
+admin.site.register(Tag)
