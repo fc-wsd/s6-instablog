cdd44920b2e9a93f2a4bd7d4ee652b5c1377b557
zeropol2 <zeropol2@gmail.com>
2016-06-10 19:46:42 +0900

    [홍창우] 어드민 슈퍼유저 생성, Comment 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,20 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created_at', '-pk']   # 정렬조건
+
+
+class Comment(models.Model):
+    _status = (
+        ('public', 'Public Post',),
+        ('private', 'Private Post',),
+    )
+    post = models.ForeignKey(Post)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)  # 생성 될 때만 값을 넣어준다
+    updated_at = models.DateTimeField(auto_now=True)  # 항상 값을 넣어준다
+    # 공개댓글 비공개댓글 설정,
+    # 추후 이웃 공개 기능 등이 들어갈 수 있으므로 boolean 이 아닌 enum으로 처리
+    status = models.CharField(max_length=20, choices=_status)
+
+    def __str__(self):
+        return '{}: {}'.format(self.pk, self.content)
