2e8dc4d4ad8f0eee7e60041f0cc4686ad83aff13
zeropol2 <zeropol2@gmail.com>
2016-06-10 19:41:01 +0900

    [홍창우]Post 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    _status = (
+        ('public', 'Public Post',),
+        ('private', 'Private Post',),
+    )
+    title = models.CharField(max_length=200)
+    content = models.TextField();
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # 공개글 비공개글 설정,
+    # 추후 이웃 공개 기능 등이 들어갈 수 있으므로 boolean 이 아닌 enum으로 처리
+    status = models.CharField(max_length=20, choices=_status)
+
+    def __str__(self):
+        return '{}: {}'.format(self.pk, self.content)
+
+    class Meta:
+        ordering = ['-created_at', '-pk']   # 정렬조건
