bb2a31c7d920fc5cce458b3ac7bd147ad62eb0a3
Kay Cha <kay@hannal.net>
2016-06-11 15:30:28 +0900

    간단한 pagination

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    url(r'^posts/(?P<pk>[0-9]+)/$', views.detail_post),
-    url(r'^$', views.list_posts),
+    url(r'^posts/(?P<pk>[0-9]+)/$', views.detail_post, name='detail'),
+    url(r'^$', views.list_posts, name='list'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
 
 from .models import Post
 
 
 def list_posts(request):
+    per_page = 2
+    # if 'page' in request.GET:
+    #     page = request.GET['page']
+    # else:
+    #     page = 1
+    page = request.GET.get('page', 1)
+    try:
+        page = int(page)
+    except ValueError:
+        return redirect('blog:list')
+        # page = 1
+
+    page = page if page >= 1 else 1
+
     posts = Post.objects.all()
+    posts = posts[(page-1)*per_page:page*per_page]
+
 
     ctx = {
         'object_list': posts,
