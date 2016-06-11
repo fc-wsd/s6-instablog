58d276d04cb478db59618158379c2a4c748dee90
commonlife <commonlife@gmail.com>
2016-06-11 15:15:31 +0900

    url 관련 정보 추가

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    url(r'^post/([0-9]+)/$', views.detail_post),
-    url(r'^$', views.list_posts),
+    # url(r'^post/(?P<pk>[0-9]+)/$', views.detail_post, name='detail'),
+    # pk라는 이름을 직접 명시 해준다. views.py의 method parameter의 순서들을 바꿀 수 있다.
+    # 이렇게 할 때 list.html에서 <a href>를 사용할 때 pk로 명시해준다.
+    url(r'^post/([0-9]+)/$', views.detail_post, name='detail'),
+    url(r'^$', views.list_posts, name='list'),
 ]
