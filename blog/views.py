92fbb3212a5eba6a362ed664e9bad53be7337771
zeropol2 <zeropol2@gmail.com>
2016-06-16 22:47:55 +0900

    [홍창우]새글 쓰기 화면 추가

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from .models import Post
 
@@ -31,3 +32,23 @@ def detail_post(request, pk):
         'post': post,
     }
     return render(request, 'detail.html', ctx)
+
+
+def create_post(request):
+    ctx = {}
+    if request.method == 'GET':
+        return render(request, 'edit.html', ctx)
+    elif request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        new_post = Post()
+        new_post.title = title
+        new_post.content = content
+        new_post.save()
+
+        url = reverse('blog:detail', kwargs={'pk': new_post.pk})
+        return redirect(url)
+
+    return render(request, 'edit.html', ctx)
+
