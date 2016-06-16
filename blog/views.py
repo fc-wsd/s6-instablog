b0023008edca2f7b3f6c536ffeb79b1d3083a91b
zeropol2 <zeropol2@gmail.com>
2016-06-16 20:14:32 +0900

    [홍창우]간단한 페이지네이션 추가

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render, redirect
 from .models import Post
 
 
 def list_posts(request):
+    per_page = 2
+    page = request.GET.get('page', 1)
+
     posts = Post.objects.all()
+    pg = Paginator(posts, per_page)
+
+    try:
+        contents = pg.page(page)
+    except PageNotAnInteger:
+        contents = pg.page(1)
+    except EmptyPage:
+        contents = []
 
     ctx = {
-        'posts': posts,
+        'posts': contents,
     }
 
     return render(request, 'list.html', ctx)
