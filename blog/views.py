b4e4eabce5dded9ac9c7573ea6e2eda7ea1bfaf1
zeropol2 <zeropol2@gmail.com>
2016-06-17 23:05:01 +0900

    [홍창우] 댓글 입력/삭제 추가

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
-from .models import Post
+from .models import Post, Comment
 
 
 def list_posts(request):
@@ -27,9 +27,11 @@ def list_posts(request):
 
 def detail_post(request, pk):
     post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
 
     ctx = {
         'post': post,
+        'comments': comments,
     }
     return render(request, 'detail.html', ctx)
 
@@ -52,3 +54,24 @@ def create_post(request):
 
     return render(request, 'edit.html', ctx)
 
+
+def create_comment(request):
+    post_pk = request.POST.get('post_pk')
+    content = request.POST.get('content')
+
+    post = Post.objects.get(pk=post_pk)
+    new_comment = Comment()
+    new_comment.post = post
+    new_comment.content = content
+    new_comment.save()
+
+    url = reverse('blog:detail', kwargs={'pk': post_pk})
+    return redirect(url)
+
+
+def delete_comment(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    post = comment.post
+    comment.delete()
+    url = reverse('blog:detail', kwargs={'pk': post.pk})
+    return redirect(url)
