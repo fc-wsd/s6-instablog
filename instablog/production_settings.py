91ca15f0fed63032f9b6430f6c00384beb02509d
Jaemin <jaemin0216@gmail.com>
2016-07-09 17:43:31 +0900

    modift production_settings.py

--- instablog/production_settings.py
+++ instablog/production_settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'testDB',
-        'HOST': 'RDS endpoint URI',
+        'HOST': 'testdb.ccwq22qn0t9j.ap-northeast-1.rds.amazonaws.com',
         'USER': 'jaemin',
         'PASSWORD': os.environ.get('PYSTAGRAM_DB_PW'),
     }
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #리스트나 튜플로 만든다
 STATICFILES_DIRS = (
